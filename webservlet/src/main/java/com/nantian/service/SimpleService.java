edf02477c977feed2c44ecab8ad275864a369486
boyajing <13971437800@163.com>
2017-08-15 17:36:46 +0800

    create simpleService

---
+++ webservlet/src/main/java/com/nantian/service/SimpleService.java
@@ -0,0 +1,10 @@
+package com.nantian.service;
+
+/**
+ * Created by nantian on 2017/8/15.
+ */
+public class SimpleService {
+    public String getSimpleService(){
+        return "this is a SimpleService";
+    }
+}
